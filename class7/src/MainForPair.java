4895d9d8b9cd983140ed3948be9395fc5aef4420
sagi9 <sagi9@Sagie197.lan>
2024-06-10 11:28:58 +0300

    yy

--- class7/src/MainForPair.java
+++ class7/src/MainForPair.java
@@ -3,7 +3,7 @@ public class MainForPair {
 		Pair<String, Integer> p1 = 
 			new OrderedPair<String, Integer>("Even", 8);
 		Pair<String, String>  p2 = 
-			new OrderedPair<String, String>("hello", "world");
+			new OrderedPair<String, String>("hello", "world!");
 		Pair<String,String> p3 = 
 			new OrderedPair<>("oh my", "god");
 		Pair<String, Box<Integer>> p = 
