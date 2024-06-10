508df5a5e81a15d4d843cec1ac0eb9d268957032
sagi9 <sagi9@Sagie197.lan>
2024-06-10 11:24:49 +0300

    name

--- class7/src/MainForNames.java
+++ class7/src/MainForNames.java
@@ -4,7 +4,7 @@ public class MainForNames {
 	public static void main(String[] args) {
 		Names<Student> names = new Names<>();
 		names.add(new Student("Bor", "Hagibor", 33));
-		names.add(new Student("Mor", "Hashikor", 111));
+		names.add(new Student("Or", "Hashikor", 111));
 		System.out.println(names.allNames());
 		// Bor Hagibor Mor Hashikor 
 	}
